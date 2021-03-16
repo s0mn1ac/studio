717054fbbb22ef25a9a81a74a6df93218f625105
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-03-16 20:22:54 +0000

    fix: actualización de estilos en la página de política de cookies

--- src/app/shared/components/footer/cookie-policy/cookie-policy.component.ts
+++ src/app/shared/components/footer/cookie-policy/cookie-policy.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CookiePolicyComponent implements OnInit {
 
-  public isDialogVisible = false;
+  public isDialogVisible = true;
 
   constructor() { }
 
