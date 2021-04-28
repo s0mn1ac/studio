be6ba5b03dad6baea8e4bd15b967bd2b0c094ae3
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-04-28 19:20:05 +0100

    add: añadidas nuevas imágenes y borradas las antiguas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainPageComponent } from './main-page/components/main-page.component';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 
 const routes: Routes = [
@@ -12,38 +11,6 @@ const routes: Routes = [
                 path: '',
                 loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)
             }
-            // {
-            //     path: 'home',
-            //     loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)
-            // },
-            // {
-            //     path: 'about',
-            //     loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)
-            // },
-            // {
-            //     path: 'gallery',
-            //     loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)
-            // },
-            // {
-            //     path: 'contact',
-            //     loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule)
-            // },
-            // // {
-            // //     path: 'main',
-            // //     loadChildren: () =>
-            // //         import('./main-page/main-page.module').then(
-            // //             m => m.MainPageModule
-            // //         )
-            // // },
-            // {
-            //     path: 'projects',
-            //     loadChildren: () => import('./projects-page/projects-page.module').then(m => m.ProjectsPageModule)
-            // }
-            // {
-            //     path: '',
-            //     redirectTo: 'home',
-            //     pathMatch: 'full'
-            // }
         ]
     }
 ];
