b84c02b1b5092ef7001e4dcb8fe680225b4cd23b
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-04-28 18:46:34 +0100

    add: añadida una galería a las miniaturas del footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { Gallery } from 'angular-gallery';
 import { SectionName } from '../../enums/section-name.enum';
 import { HeaderItem } from '../../models/header-item.model';
+import { ImageItem } from '../../models/image-item.model';
 import { AppService } from '../../services/app.service';
 import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
 import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
@@ -26,14 +28,19 @@ export class FooterComponent implements OnInit {
 
   public footerItems!: HeaderItem[];
 
-  public images: any[] = [];
+  public imagesFirstRow: ImageItem[] = [];
+  public imagesSecondRow: ImageItem[] = [];
 
-  constructor(private appService: AppService) { }
+  public images!: ImageItem[];
+
+  private properties!: any;
+
+  constructor(private appService: AppService, private gallery: Gallery) { }
 
   ngOnInit(): void {
     this.setCopyYear();
     this.initFooterItems();
-    this.getImages();
+    this.initImageItems();
   }
 
   public onClickFooterOption(name: string): void {
@@ -69,15 +76,31 @@ export class FooterComponent implements OnInit {
 
   }
 
-  private getImages(): void {
+  private initImageItems(): void {
 
-    for (let i = 0; i < 6; i++) {
-      this.images.push({
-        title: `image${i}`,
-        url: `url(../../../../assets/images/miniatures/full-size/image-${i}.jpeg)`,
-      });
-    }
+    this.properties = { images: [], index: 0 };
 
+    this.imagesFirstRow = [
+      { id: 0, active: true, title: 'Title', description: 'Description', thumbnail: '../../../../assets/images/gallery/image-0.jpeg', fullSize: '../../../../assets/images/gallery/image-0.jpeg' },
+      { id: 1, active: true, title: 'Title', description: 'Description', thumbnail: '../../../../assets/images/gallery/image-1.jpeg', fullSize: '../../../../assets/images/gallery/image-1.jpeg' },
+      { id: 2, active: true, title: 'Title', description: 'Description', thumbnail: '../../../../assets/images/gallery/image-2.jpeg', fullSize: '../../../../assets/images/gallery/image-2.jpeg' }
+    ];
+
+    this.imagesSecondRow = [
+      { id: 3, active: true, title: 'Title', description: 'Description', thumbnail: '../../../../assets/images/gallery/image-3.jpeg', fullSize: '../../../../assets/images/gallery/image-3.jpeg' },
+      { id: 4, active: true, title: 'Title', description: 'Description', thumbnail: '../../../../assets/images/gallery/image-4.jpeg', fullSize: '../../../../assets/images/gallery/image-4.jpeg' },
+      { id: 5, active: true, title: 'Title', description: 'Description', thumbnail: '../../../../assets/images/gallery/image-5.jpeg', fullSize: '../../../../assets/images/gallery/image-5.jpeg' }
+    ];
+    
+    this.images = this.imagesFirstRow.concat(this.imagesSecondRow);
+
+    this.images.forEach((imageItem: ImageItem) => this.properties.images.push({ path: imageItem.fullSize }));
+  }
+
+  public showGallery(route: string): void {
+    const index =  this.properties.images.findIndex((image: any) => image.path === route);
+    this.properties.index = index;
+    this.gallery.load(this.properties);
   }
 
 }
