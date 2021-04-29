3994730a2ecf8aa16b332322216e08582d54882e
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-04-29 17:58:45 +0100

    wip: corrección del video de presentación

--- src/app/main-page/components/home/home.component.ts
+++ src/app/main-page/components/home/home.component.ts
@@ -1,17 +1,30 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('presentationVideo') presentationVideo!: HTMLMediaElement;
 
   @Output() scrollDownEmitter: EventEmitter<void> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void { }
+
+  ngAfterViewInit() {
+    // const video = document.getElementById('presentationVideo');
+    
+    // if (!this.presentationVideo.played) {
+    //   this.presentationVideo.play();
+    //   console.log('En pausa')
+    // } else {
+    //   console.log('En play')
+    // }
+  }
 
   public scrollDown(): void {
     this.scrollDownEmitter.emit();
