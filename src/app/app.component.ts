d35e692393c5d2c20419bcb55c062f65a56d9df7
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-05-11 19:14:13 +0100

    wip: pop-up para el consentimiento de cookies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ThemeType } from './shared/enums/theme-type.enum';
 import { OverlayContainer } from '@angular/cdk/overlay';
+import { Subscription } from 'rxjs';
+import { NgcCookieConsentService, NgcInitializeEvent, NgcNoCookieLawEvent, NgcStatusChangeEvent } from 'ngx-cookieconsent';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+
+  private popupOpenSubscription$!: Subscription;
+  private popupCloseSubscription$!: Subscription;
+  private initializeSubscription$!: Subscription;
+  private statusChangeSubscription$!: Subscription;
+  private revokeChoiceSubscription$!: Subscription;
+  private noCookieLawSubscription$!: Subscription;
 
   @HostBinding('class') public cssClass!: string;
 
-  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, private overlayContainer: OverlayContainer) {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, private overlayContainer: OverlayContainer, private ccService: NgcCookieConsentService) {
     this.matIconRegistry.addSvgIcon('custom-flag-es', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/flags/spain-flag.svg'));
     this.matIconRegistry.addSvgIcon('custom-flag-en', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/flags/united-kingdom-flag.svg'));
     this.matIconRegistry.addSvgIcon('custom-flag-de', this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/icons/flags/germany-flag.svg'));
@@ -21,6 +30,25 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.overlayContainer.getContainerElement().classList.add(ThemeType.DARK);
     this.cssClass = ThemeType.DARK;
+    this.initCookieSubscription();
+  }
+
+  private initCookieSubscription(): void {
+    this.popupOpenSubscription$ = this.ccService.popupOpen$.subscribe(() => { });
+    this.popupCloseSubscription$ = this.ccService.popupClose$.subscribe(() => { });
+    this.initializeSubscription$ = this.ccService.initialize$.subscribe((event: NgcInitializeEvent) => {  });
+    this.statusChangeSubscription$ = this.ccService.statusChange$.subscribe((event: NgcStatusChangeEvent) => { this.ccService.getStatus().allow; console.log(event) });
+    this.revokeChoiceSubscription$ = this.ccService.revokeChoice$.subscribe(() => { });
+    this.noCookieLawSubscription$ = this.ccService.noCookieLaw$.subscribe((event: NgcNoCookieLawEvent) => {  });
+  }
+
+  ngOnDestroy(): void {
+    this.popupOpenSubscription$.unsubscribe();
+    this.popupCloseSubscription$.unsubscribe();
+    this.initializeSubscription$.unsubscribe();
+    this.statusChangeSubscription$.unsubscribe();
+    this.revokeChoiceSubscription$.unsubscribe();
+    this.noCookieLawSubscription$.unsubscribe();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,41 @@ import { GlobalErrorHandler } from './config/error-handler.service';
 import { TranslocoInitializer } from './config/initializers/transloco.initializer';
 import { MaterialModule } from './shared/modules/material/material.module';
 import { IvyGalleryModule } from 'angular-gallery';
+import { NgcCookieConsentConfig, NgcCookieConsentModule } from 'ngx-cookieconsent';
+import { environment } from './../environments/environment';
+
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: environment.cookieDomain
+  },
+  palette: {
+    popup: {
+      background: '#065A82',
+      text: '#FFFFFF',
+      link: '#FFFFFF'
+    },
+    button: {
+      background: '#F4AC45',
+      text: '#FFFFFF',
+      border: 'transparent'
+    }
+  },
+  content: {
+    message: 'Este sitio web utiliza cookies para mejorar tu experiencia de usuario.',
+    // message: 'This website uses cookies to ensure you get the best user experience.',
+    link: 'Saber más',
+    dismiss: '¡Vamos allá!'
+  },
+  theme: 'classic',
+  position: 'bottom',
+  type: 'info'
+};
 
 @NgModule({
   declarations: [AppComponent],
   exports: [BrowserAnimationsModule],
   imports: [
+    NgcCookieConsentModule.forRoot(cookieConfig),
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
