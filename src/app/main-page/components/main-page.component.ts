21163c86797e38bb0bc1b1f7d127243014d2b4d5
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2021-05-11 17:53:26 +0100

    wip: animaciones

--- src/app/main-page/components/main-page.component.ts
+++ src/app/main-page/components/main-page.component.ts
@@ -22,7 +22,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
   constructor(private appService: AppService, private router: Router) { }
 
   ngOnInit(): void {
-    AOS.init({ once: true });
+    AOS.init({ once: false, duration: 1000 });
     this.initSubscriptions();
   }
 
